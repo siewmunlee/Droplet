2a00339e141d1d0c9c5d40e8a03e82cc8d9078ce
Team2.Siewmun <2355355L@student.gla.ac.uk>
2018-11-21 17:25:58 +0800

    fuck yeah chat

--- Droplet/src/screens/inbox/chat.js
+++ Droplet/src/screens/inbox/chat.js
@@ -1,25 +1,65 @@
 import React, { Component } from "react";
 import { GiftedChat } from 'react-native-gifted-chat'
-import { Container, Header, H3, Title, Content, Button, Item, Label, Input, Body, Left, Right, Icon, Form, Text} from "native-base";
+import Fire from './Fire';
+import { Container, Header, H3, Title, Content, Button, Item, Label, Input, Body, Left, Right, Icon, Text } from "native-base";
 
-class FloatingLabel extends Component {
+const useracc = {
+  name: "Fam"
+}
+
+class Chat extends Component {
+  state = {
+    messages: []
+  };
+
+  get user() {
+    return {
+      name: useracc.name,
+      _id: Fire.shared.uid,
+    };
+  }
+
+
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent onPress={() => this.props.navigation.goBack()}>
-              <Icon name="arrow-back" />
-            </Button>
-          </Left>
-          <Body>
-            <Title><H3>Chat</H3></Title>
-          </Body>
-          <Right />
-        </Header>
-      </Container>
+      // <Container>
+      //   <Header>
+      //     <Left>
+      //       <Button transparent onPress={() => this.props.navigation.goBack()}>
+      //         <Icon name="arrow-back" />
+      //       </Button>
+      //     </Left>
+      //     <Body>
+      //       <Title><H3>Chat</H3></Title>
+      //     </Body>
+      //     <Right />
+      //   </Header>
+      //   <Content>
+      //     <GiftedChat
+      //       messages={this.state.messages}
+      //       onSend={Fire.shared.send}
+      //       user={this.user}
+      //     />
+      //   </Content>
+      // </Container>
+      <GiftedChat
+      messages={this.state.messages}
+      onSend={Fire.shared.send}
+      user={this.user}
+    />
     );
+  }
+
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+  componentWillUnmount() {
+    Fire.shared.off();
   }
 }
 
-export default FloatingLabel;
+export default Chat;
