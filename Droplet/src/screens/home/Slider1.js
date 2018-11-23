66deee5faf65c91c7efcd4953fd098b5076a8c97
DESKTOP-DV3JCHC\Rafi <2355370R@student.gla.ac.uk>
2018-11-23 15:35:03 +0800

    Incorporate Slider into App

---
+++ Droplet/src/screens/home/Slider1.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import {StyleSheet,View,Slider} from 'react-native';
+import { Container, Header, H3, Title, Content, Button, Item, Label, Input, Body, Left, Right, Icon, Text } from "native-base";
+export default class Slider1 extends React.Component  {
+  constructor(props) {
+   super(props)
+   this.state = { age: 45 , age1:45,age2: 45 , age3:45,age4: 45 , age5:45}
+  }
+  getVal(val){
+  //console.warn(val);
+  }
+  render() {
+
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent onPress={() => this.props.navigation.goBack()}>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title><H3>Emotion</H3></Title>
+          </Body>
+          <Right />
+        </Header>
+        <View style={styles.container}>
+          <Slider
+           style={{ width: 300 }}
+           step={1}
+           minimumValue={18}
+           maximumValue={71}
+           value={this.state.age}
+           onValueChange={val => this.setState({ age: val })}
+           onSlidingComplete={ val => this.getVal(val)}
+          />
+          <Text style={styles.welcome}>
+            {this.state.age}
+          </Text>
+          <Text style={styles.instructions}>
+            Worried -------- Untroubled
+          </Text>
+
+      <Slider
+           style={{ width: 300 }}
+           step={1}
+           minimumValue={18}
+           maximumValue={71}
+           value={this.state.age1}
+           onValueChange={val => this.setState({ age1: val })}
+           onSlidingComplete={ val => this.getVal(val)}
+          />
+          <Text style={styles.welcome}>
+            {this.state.age1}
+          </Text>
+        <Text style={styles.instructions}>
+            Pressured -------- Calm
+          </Text>
+
+      <Slider
+           style={{ width: 300 }}
+           step={1}
+           minimumValue={18}
+           maximumValue={71}
+           value={this.state.age2}
+           onValueChange={val => this.setState({ age2: val })}
+           onSlidingComplete={ val => this.getVal(val)}
+          />
+          <Text style={styles.welcome}>
+            {this.state.age2}
+          </Text>
+        <Text style={styles.instructions}>
+           Tense -------- Relaxed
+          </Text>
+
+      <Slider
+           style={{ width: 300 }}
+           step={1}
+           minimumValue={18}
+           maximumValue={71}
+           value={this.state.age3}
+           onValueChange={val => this.setState({ age3: val })}
+           onSlidingComplete={ val => this.getVal(val)}
+          />
+          <Text style={styles.welcome}>
+            {this.state.age3}
+          </Text>
+        <Text style={styles.instructions}>
+            Sleepless -------- Rested
+          </Text>
+
+      <Slider
+           style={{ width: 300 }}
+           step={1}
+           minimumValue={18}
+           maximumValue={71}
+           value={this.state.age4}
+           onValueChange={val => this.setState({ age4: val })}
+           onSlidingComplete={ val => this.getVal(val)}
+          />
+          <Text style={styles.welcome}>
+            {this.state.age4}
+          </Text>
+        <Text style={styles.instructions}>
+            Distracted -------- Focused
+          </Text>
+
+      <Slider
+           style={{ width: 300 }}
+           step={1}
+           minimumValue={18}
+           maximumValue={71}
+           value={this.state.age5}
+           onValueChange={val => this.setState({ age5: val })}
+           onSlidingComplete={ val => this.getVal(val)}
+          />
+          <Text style={styles.welcome}>
+            {this.state.age5}
+          </Text>
+        <Text style={styles.instructions}>
+            Irritable -------- Cheerful
+          </Text>
+
+
+
+        </View>
+      </Container>
+
+
+
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- Droplet/src/screens/home/index.js
+++ Droplet/src/screens/home/index.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
-import { ImageBackground, View, StatusBar } from "react-native";
-import { Container, Button, H3, Text, Title, Header, Icon, Left, Body, Right } from "native-base";
+import {StyleSheet,View,TouchableOpacity,Image,Alert,ScrollView,FlatList} from 'react-native';
+import { Container, Button, H3, Text, Title, Header, Icon, Left, Body, Right,Content} from "native-base";
 import { DrawerActions } from 'react-navigation-drawer';
 
+
+const datas =
+    {
+      route: "Slider1",
+      text: "Counsellor"
+    }
+  ;
+
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {id:1, title: "Anxiety",      color:"#FF4500",   image:"https://img.icons8.com/color/70/000000/name.png"},
+        {id:1, title: "Depression",     color:"#87CEEB",   image:"https://img.icons8.com/office/70/000000/home-page.png"},
+        {id:2, title: "General Well-Being",     color:"#4682B4",  image:"https://img.icons8.com/color/70/000000/two-hearts.png"} ,
+        {id:3, title: "Stress",   color:"#6A5ACD",   image:"https://img.icons8.com/color/70/000000/family.png"} ,
+
+      ]
+    };
+  }
+
+  clickEventListener(item) {
+    Alert.Alert(item.title)
+  }
     render() {
         return (
             <Container>
@@ -21,7 +45,95 @@ export default class Home extends Component {
                     </Body>
                     <Right />
                 </Header>
+
+
+                <View style={styles.container}>
+                  <FlatList style={styles.list}
+                    contentContainerStyle={styles.listContainer}
+                    data={this.state.data}
+                    horizontal={false}
+                    numColumns={2}
+                    keyExtractor= {(item) => {
+                      return item.id;
+                    }}
+                    renderItem={({item}) => {
+                      return (
+                        <TouchableOpacity style={[styles.card, {backgroundColor:item.color}]} onPress={() =>  this.props.navigation.navigate(datas.route)}>
+                          <View style={styles.cardHeader}>
+                            <Text style={styles.title}>{item.title}</Text>
+                          </View>
+                          <Image style={styles.cardImage} source={{uri:item.image}}/>
+                          <View style={styles.cardFooter}>
+                            <Text style={styles.subTitle}></Text>
+                          </View>
+                        </TouchableOpacity>
+                      )
+                    }}/>
+                </View>
+
             </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop:20,
+  },
+  list: {
+    //paddingHorizontal: 5,
+    backgroundColor:"#E6E6E6",
+  },
+  listContainer:{
+    alignItems:'center'
+  },
+  /******** card **************/
+  card:{
+    marginHorizontal:2,
+    marginVertical:2,
+    flexBasis: '48%',
+  },
+  cardHeader: {
+    paddingVertical: 17,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 1,
+    borderTopRightRadius: 1,
+    flexDirection: 'row',
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  cardContent: {
+    paddingVertical: 12.5,
+    paddingHorizontal: 16,
+  },
+  cardFooter:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 12.5,
+    paddingBottom: 25,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 1,
+    borderBottomRightRadius: 1,
+  },
+  cardImage:{
+    height: 70,
+    width: 70,
+    alignSelf:'center'
+  },
+  title:{
+    fontSize:16,
+    flex:1,
+    color:"#FFFFFF",
+    fontWeight:'bold'
+  },
+  subTitle:{
+    fontSize:12,
+    flex:1,
+    color:"#FFFFFF",
+  },
+  icon:{
+    height: 20,
+    width: 20,
+  }
+});
